3a145ffec5ed270ae6609b2ed1618a70e6815a13
Mads Akselsen <akselsenmads@gmail.com>
2020-05-05 21:21:09 +0200

    added in progress code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ListItems from "./ListItems";
+
+import "./App.css";
 
 function App() {
+  const [todo, setTodo] = useState({
+    items: [],
+    currentItem: {
+      text: "",
+      key: "",
+    },
+  });
+
+  const handleChange = (e) => {
+    setTodo({
+      ...todo,
+      currentItem: {
+        ...todo.currentItem,
+        text: e.target.value,
+        key: Date.now(),
+      },
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newItem = todo.currentItem;
+    console.log(newItem);
+    if (newItem.text !== "") {
+      const newItems = [...todo.items, newItem];
+      setTodo({
+        ...todo,
+        items: newItems,
+        currentItem: {
+          ...todo.currentItem,
+          text: "",
+          key: "",
+        },
+      });
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <form onSubmit={handleSubmit} id="to-do-form">
+          <input
+            type="text"
+            placeholder="Enter Text"
+            value={todo.currentItem.text}
+            onChange={handleChange}
+          />
+          <button type="submit">Add</button>
+        </form>
       </header>
     </div>
   );
---
+++ src/ListItems.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./ListItems.css";
+
+const ListItems = (props) => {
+  return <h1>From ListItems.js</h1>;
+};
